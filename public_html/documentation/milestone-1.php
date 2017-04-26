cf5a4b6d4605c03a8a3a6597d6f2fd63b8d14999
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-26 11:43:33 -0600

    updated milestone 1 php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<meta charset="utf-8">
+
+				<title>MILESTONE 1</title>
+
+	</head>
+
+	<body>
+		<header>
+			<h1>MILESTONE 1</h1>
+		</header>
+
+		<main>
+
+		</main>
+	</body>
+</html>
