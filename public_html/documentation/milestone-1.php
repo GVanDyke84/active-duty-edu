f531526e9bd7edbea9185e8d80c828cd57222ac3
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-26 21:37:50 -0600

    added persona to milestone-1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -27,6 +27,14 @@
 				Goal: My goal is twofold. As a developer I plan on showcasing this site as part of my future web developer portfolio which will give potential clients a good idea of my capabilities. I also am in the process of founding a non-profit organization to help active duty military members and veterans maximize their education benefits, and I plan on this site serving as a gateway to future digital products outlining concrete degree paths.
 			</p>
 
+		<p>
+			Persona: Jane Johnson 20 years old and is in her 2nd year of service in the Army. She has her own laptop, but when on deployment she is only able to connect to the internet on secure Army computers. She wants information on the best DANTES/CLEP exams to take and how they will transfer into a degree program of her choice at well recognized universities. Time on secure computers is limited, with many other people in line to use them, so she would like to get thorough information as quickly as possible.
+		</p>
+
+			<p>
+
+			</p>
+
 		</main>
 	</body>
 </html>
