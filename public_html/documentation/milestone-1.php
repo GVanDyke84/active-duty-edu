fc35ef7ea1837e53ef03b9f18550754140b1876b
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-26 21:21:35 -0600

    added audience and goals to milestone-1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,10 +16,16 @@
 		<main>
 
 			<p>
-				Purpose: I have become quite knowledgeable about educational benefits for military members and I intend for this site 				to serve as an informational resource to guide and help military members looking to maximize the educational benefits 				available to them.
+				Purpose: I have become quite knowledgeable about educational benefits for military members and I intend for this site to serve as an informational resource to guide and help military members looking to maximize the educational benefits available to them.
 			</p>
 
+			<p>
+				Audience: This site is geared towards active duty military members motivated to complete between 30-50 college credits before they separate from active duty and start using their G.I. Bill benefits.
+			</p>
 
+			<p>
+				Goal: My goal is twofold. As a developer I plan on showcasing this site as part of my future web developer portfolio which will give potential clients a good idea of my capabilities. I also am in the process of founding a non-profit organization to help active duty military members and veterans maximize their education benefits, and I plan on this site serving as a gateway to future digital products outlining concrete degree paths.
+			</p>
 
 		</main>
 	</body>
