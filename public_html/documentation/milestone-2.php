178084fa2b0f2c17b289769b348c97521d4691ac
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-05-13 14:10:23 -0600

    added images for mobile wireframes and pc wireframe

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -17,6 +17,12 @@
 
 			<p>Content Strategy: The top of my page will contain an introduction that details the mission statement of Active Duty Edu and explains the educational opportunities available to military members in a short and very clear manner. The first table that I have will contain the names of the most common CLEP courses that translate into college credits at a wide variety of universities. The second table will be a sample that shows the format of the digital products that my non-profit will be offering in the future. The call to action for my re-captcha mail form will give users the opportunity to receive updates as to when future guides will be released and the opportunity to receive discount codes for these products.</p>
 
+			<label>Mobile wireframes</label>
+			<img src="images/mobilewf1.jpg" alt="1st page of mobile wireframe">
+			<img src="images/mobilewf2.jpg" alt="2nd page of mobile wireframe">
+			<label>PC wireframe</label>
+			<img src="images/pcwf.jpg" alt="PC wireframe">
+
 
 
 		</main>
