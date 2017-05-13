77175c65027ddc3c6dbf0b73ed6fb7f610de0737
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-05-13 13:20:44 -0600

    created milestone 2 file and added my content strategy

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="utf-8">
+
+		<title>MILESTONE 2</title>
+
+	</head>
+
+	<body>
+		<header>
+			<h1>MILESTONE 2</h1>
+		</header>
+
+		<main>
+
+			<p>Content Strategy: The top of my page will contain an introduction that details the mission statement of Active Duty Edu and explains the educational opportunities available to military members in a short and very clear manner. The first table that I have will contain the names of the most common CLEP courses that translate into college credits at a wide variety of universities. The second table will be a sample that shows the format of the digital products that my non-profit will be offering in the future. The call to action for my re-captcha mail form will give users the opportunity to receive updates as to when future guides will be released and the opportunity to receive discount codes for these products.</p>
+
+
+
+		</main>
+	</body>
+</html>
